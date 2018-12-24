b5a03f5ae72e064cf4ae7a04b551e86abe12bf19
IsaiasPC <isamed92@gmail.com>
2018-12-24 03:10:18 -0300

    added inital structure

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+
+export const ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'search', component: SearchComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'home'},
+    { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
