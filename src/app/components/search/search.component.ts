feddebba84c9de8b3e51afdad8a64d957b2070c2
IsaiasPC <isamed92@gmail.com>
2018-12-26 01:21:10 -0300

    adding functionality of search artist

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styles: []
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent  {
 
-  constructor() { }
+  artistas: any[] = [];
+  constructor(
+    private spotify: SpotifyService
+  ) { }
 
-  ngOnInit() {
-  }
+ buscar(entrada: string) {
+  console.log(entrada);
+  this.spotify.getArtista(entrada)
+    .subscribe((data: any) => {
+      console.log(data.artists.items);
+      this.artistas = data.artists.items;
+    });
+ }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -21,4 +21,12 @@ export class SpotifyService {
       .get('https://api.spotify.com/v1/browse/new-releases', { headers });
 
   }
+
+  getArtista(terminoBusqueda: string) {
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQA1uagrY1TH6w7qNrO9QboxgDM19fJzs1PWxmkYcWKEf9o1BGeg0yvd447eRJQOgKk8zdSI7hV9NARZOzg'
+    });
+    return this.http
+      .get(`https://api.spotify.com/v1/search?q=${ terminoBusqueda }&type=artist&limit=15`, { headers });
+  }
 }
